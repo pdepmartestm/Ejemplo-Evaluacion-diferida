b37b14ef2bb641d977649ee20cab7cb9ce0612eb
lspigariol <lspigariol@gmail.com>
2020-06-05 19:09:57 -0300

    ajustes en clase

--- ejemplo - recursividad listas infinitas - numeros.hs
+++ ejemplo - recursividad listas infinitas - numeros.hs
@@ -3,8 +3,11 @@
 
 -- Temática: Números 
 
+
 factorial 0 = 1
-factorial n = n * factorial (n-1)
+factorial n | n <0 = error " los negativos no tienen factorial"
+           | otherwise=  n * factorial (n-1)
+
 
 f1 0 = 0
 f1 1 = 1
@@ -15,18 +18,26 @@ f1 x | x > 0 = f1 (x - 1) + f1 (x-2) + 1
 sumatoria [] = 0
 sumatoria (x:xs) = x + sumatoria xs
 
+
+
+
+
 -- igual a length
 largo [] = 0
-largo (x:xs) = 1 + largo xs
+largo (_:xs) = 1 + largo xs
 
 -- igual a ^
+potencia 0 0 = error ""
 potencia a 0 = 1
 potencia a b = a * potencia a (b - 1)
+
+--f x = h x + g x
 
 takeHasta nro [] = []
 takeHasta tope (x:xs)
  | x > tope = []
  | otherwise = x : takeHasta tope xs
+
 
 -- igual a take
 takeCant nro [] = []
@@ -34,10 +45,13 @@ takeCant 0 _ = []
 takeCant nro (x:xs) = x : takeCant (nro - 1) xs
 
 
-funcionLoca x = x : funcionLoca (x-1)
---funcionLoca x = x : funcionLoca (x*2)
---funcionLoca x = x : funcionLoca x
+funcionLoca1 x = x : funcionLoca1 (x+1)
+funcionLoca2 x = x : funcionLoca2 (x*2)
 
+funcionLoca x = x : funcionLoca x
+
+
+tomarTantos x =  take x (funcionLoca2 1) 
 
 -- Otros ejemplos 
 ---  
